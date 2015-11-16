fec11d10cc4383893bb0139cc1b391b29e4afd48
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-11-16 00:13:38 -0500

    Ages, arrays

---
+++ pset2/ages.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+		int n;
+		do {
+				printf("Number of people in room: ");
+				n = GetInt();
+		} while (n < 1);
+
+		int ages[n];
+		for (int i = 0; i < n; i++)
+		{
+				printf("Age of person #%i?", i+1);
+				int age = GetInt();
+				ages[i] = age;
+		}
+
+		printf("Time passes...\n");
+
+		for (int i = 0; i < n; i++)
+		{
+				printf("A year from now, person %i will be %i years old\n", i+1, ages[i]+1);
+		}
+}
