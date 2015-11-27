a73982da9ff2559ee514a176b49048c14b96b0bf
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-11-27 18:04:36 -0500

    implement bubble sort

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -8,7 +8,6 @@
  */
        
 #include <cs50.h>
-
 #include "helpers.h"
 
 /**
@@ -35,5 +34,20 @@ bool search(int value, int values[], int n)
 void sort(int values[], int n)
 {
     // TODO: implement an O(n^2) sorting algorithm
+		int swapped = 1;
+		do
+		{
+			swapped = 1;
+			for (int i = 0; i < n-1; i++)
+			{
+				if (values[i] > values[i+1])
+				{
+					int temp = values[i+1];
+					values[i+1] = values[i];
+					values[i] = temp;
+					swapped = 0;
+				}
+			}
+		} while (swapped != 1);
     return;
 }
