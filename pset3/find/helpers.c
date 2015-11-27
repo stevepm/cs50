f46239d994877d111d3196d2c61e1628d5a82a45
Steven Magelowitz <steven.magelowitz@gmail.com>
2015-11-27 17:28:37 -0500

    Implement linear search algorithm

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -16,7 +16,16 @@
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
+		if (n > 0)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				if (value == values[i])
+				{
+					return true;
+				}
+			}
+		}
     return false;
 }
 
